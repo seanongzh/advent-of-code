b1c4646d92702f5f537ea7158b7fd4aa9e4547bf
seanongzh <sean.ong@tufts.edu>
2020-01-05 00:10:40 -0500

    Working intcode

--- 5/intcode.py
+++ 5/intcode.py
@@ -16,15 +16,15 @@ def read(program):
       inst = program[i]
       # instruction ABCDE: DE is two-digit opcode
       opcode = inst % 100
-      # Arithmetic instructions, 3 parameters
-      if opcode == 1 or opcode == 2:
-        
-        # Parse parameter modes (ABC in instruction ABCDE)
-        fstMode = (inst // 100) % 10
-        sndMode = (inst // 1000) % 10
-        trdMode = inst // 10000
 
-        # 0 is position mode, 1 is immediate mode
+      # Parse parameter modes (ABC in instruction ABCDE)
+      # 0 is position (variable), 1 is immediate (literal)
+      fstMode = (inst // 100) % 10
+      sndMode = (inst // 1000) % 10
+      trdMode = inst // 10000
+
+      # Math instructions, 3 parameters
+      if opcode == 1 or opcode == 2 or opcode == 7 or opcode == 8:
         if fstMode == 0:
           fst = program[program[i+1]]
         elif fstMode == 1:
@@ -33,33 +33,51 @@ def read(program):
           snd = program[program[i+2]]
         elif sndMode == 1:
           snd = program[i+2]
-        if trdMode == 0:        # Can never be in immediate mode
+        if trdMode == 0:            # Can never be in immediate mode
           trd = program[i+3]
-
         if opcode == 1:             # Opcode 1 (addition)
           program[trd] = fst + snd
-        else:                       # Opcode 2 (multiplication)
+        elif opcode == 2:           # Opcode 2 (multiplication)
           program[trd] = fst * snd
-
+        elif opcode == 7:           # Opcode 7 (less than)
+          if fst < snd:
+            program[trd] = 1
+          else:
+            program[trd] = 0
+        elif opcode == 8:           # Opcode 8 (equals)
+          if fst == snd:
+            program[trd] = 1
+          else:
+            program[trd] = 0
         i += 4
       
-      # Opcode 3
-      # Input instruction, 1 parameter, no modes
-      elif opcode == 3: 
+      # I/O instructions, 1 parameter
+      elif opcode == 3 or opcode == 4: 
         param = program[i+1]
-        program[param] = int(input("Input: "))
+        if opcode == 3:                             # Opcode 3 (Input)
+          program[param] = int(input("Input: "))
+        else:                                       # Opcode 4 (Output)
+          if fstMode == 0:
+            print(program[param])
+          elif fstMode == 1:
+            print(param)
         i += 2
       
-      # Opcode 4
-      # Output instruction, 1 parameter
-      elif opcode == 4:
-        paramMode = (inst // 100) % 10
-        param = program[i+1]
-        if paramMode == 0:
-          print(program[param])
-        elif paramMode == 1:
-          print(param)
-        i += 2
+      # Conditionals, 2 parameters
+      elif opcode == 5 or opcode == 6:
+        if fstMode == 0:
+          fst = program[program[i+1]]
+        elif fstMode == 1:
+          fst = program[i+1]
+        if sndMode == 0:
+          snd = program[program[i+2]]
+        elif sndMode == 1:
+          snd = program[i+2]
+        # Opcode 5 (jump if true) and Opcode 6 (jump if false)
+        if (opcode == 5 and fst != 0) or (opcode == 6 and fst == 0):
+          i = snd
+        else:
+          i += 3
 
       # Halt instruction, no parameters
       elif opcode == 99:
